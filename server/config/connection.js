178a901b7123d4dbda42b73d397bf54d7b1ee6e5
nah <hef9874@gmail.com>
2023-05-21 16:38:00 -0400

    trying to resolve sign up issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,22 @@
-const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  }
-);
+const mongoose = require('mongoose');
 
-module.exports = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+// Handle connection events
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
+
+
